3e93ab687952ee7dda834977ac29fa3a2973318b
Igor Sivak <igorsivak97@gmail.com>
2021-10-29 11:03:40 +0300

    update bot

--- main.py
+++ main.py
@@ -1,40 +1,82 @@
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.common.by import By
 from selenium import webdriver
-import xlsxwriter
-import os
-
-driver = webdriver.Chrome()
+import pandas as pd
+import config
+import time
 
 
 def main():
+    options = webdriver.ChromeOptions()
+    prefs = {"download.default_directory": "\output"}
+    options.add_experimental_option("prefs", prefs)
+    driver = webdriver.Chrome(options=options)
 
-    # Navigate to url
+    #Navigate to url
     driver.get('https://itdashboard.gov/')
 
-    # Now click on button
+    #Time of wait
+    driver.implicitly_wait(5)
+
+    #Now click on button
     dive_button = driver.find_element(By.LINK_TEXT, value='DIVE IN')
     dive_button.click()
+    time.sleep(5)
 
-    amounts =driver.find_elements(By.XPATH, '//*[@id="agency-tiles-widget"]/div/div[*]/div[*]/div/div/div/div[1]/a/span[2]')
+    #Gets all departments and amounts -> objects
+    WebDriverWait(driver, 5).until(EC.visibility_of_element_located((By.XPATH, '//*[@id="agency-tiles-container"]')))
+    departments = driver.find_elements(By.XPATH, '//div[@id="agency-tiles-widget"]//span[@class="h4 w200"]')
+    amounts = driver.find_elements(By.XPATH, '//div[@id="agency-tiles-widget"]//span[@class=" h1 w900"]')
 
-    # Create a workbook and add a worksheet.
-    workbook = xlsxwriter.Workbook('task.xlsx')
-    worksheet = workbook.add_worksheet("Agencies")
+    agencies = [department.text for department in departments]
+    spendings = [amount.text for amount in amounts]
 
-    col = 0
-    row = 0
+    #Add data in table -> table
+    dictionary = {'department': agencies, 'amount': spendings, }
+    df = pd.DataFrame(data=dictionary).drop_duplicates(ignore_index=True)
+    df.to_excel('output/Agencies.xlsx', index=False)
 
-    # Iterate over the data and write it out row by row.
-    for date in amounts:
-        worksheet.write(row, col, date.text)
-        row += 1
-    workbook.close()
-    try:
-        agency = driver.find_element(By.PARTIAL_LINK_TEXT, os.getenv("AGENCY"))
-        agency.click()
-    except BaseException:
-        print("Error")
+    #Openening the file and work on it -> open_web_page
+    agency = departments[agencies.index(config.agency)].click()
+    time.sleep(8)
+
+    #Displaying the entire table
+    selector = driver.find_element_by_xpath(\
+        "/html/body/main/div/div/div/div[4]/div/div/div/div[2]/div/div[1]/div/div/div/div/div[2]/div[2]/div/label/select/option[4]")
+    selector.click()
+    time.sleep(10)
+
+    table = driver.find_element_by_id("investments-table-container")
+    pd_table = pd.read_html(table.get_attribute("outerHTML"))
+    pd_table[1].to_excel(f"output/{config.agency}_Individual_Investments.xlsx", index=False)
+
+    #Getting links
+    links_column = table.find_elements_by_tag_name("a")
+    links = [item.get_attribute('href') for item in links_column if item.get_attribute('href') != None]
+    time.sleep(5)
+    driver.quit()
+
+    #Downloadables file
+    for link in links:
+        driver.get(link)
+        time.sleep(5)
+        download = driver.find_element_by_xpath(\
+            '/html/body/main/div/div/div/div[1]/div/div/div/div/div[1]/div/div/div/div/div[6]/a')
+        download.click()
+        time.sleep(10)
+    #driver.quit()
 
 
-if __name__ == '__main__':
+
+
+
+
+
+
+if '__name__' == '__main__':
     main()
+
+
+
+
